927e40bd0f0c37cf7fb8340cbdb520b61af078ec
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-11-08 22:06:59 -0300

    avance app que habla

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -2,13 +2,39 @@ package com.example.helloandroidstudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    var tts: TextToSpeech? = null  //variable local del tipo TextToSpeech
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //vistas
+
+        tts = TextToSpeech(this,this) //contexto = misma actividad, listener que debe ser compatible con la interface TextToSpeech
+
+        findViewById<Button>(R.id.btnPlay).setOnClickListener{speak()}
+
     }
-    private fun variablesYConstantes(){
+
+    private fun speak(){
+        var message: String = findViewById<TextView>(R.id.textView).text.toString() //extraigo el id de la vista
+
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onInit(status: Int) {  //Método que pregunta si el movil esta habilitado para hablar
+        if(status == TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.textView).text = "Listo"
+            tts!!.language = Locale.CHINA
+        } else {
+            findViewById<TextView>(R.id.textView).text = "No disponible"
+        }
 
     }
 }
